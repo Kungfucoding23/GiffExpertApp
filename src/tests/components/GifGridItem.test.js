07bb1557ecadda01d0c8c7cebca93d9f80fcd1a6
Alejandro <alesantin012@hotmail.com>
2021-04-02 19:23:43 -0300

    tests

--- src/tests/components/GifGridItem.test.js
+++ src/tests/components/GifGridItem.test.js
@@ -4,8 +4,12 @@ import {shallow} from 'enzyme';
 import GifExpertApp from "../../components/GifExpertApp"
 
 describe('Pruebas en componente GifGridItem', () => {
+
+    const title = 'Un titulo';
+    const url = 'https://localhost/algo.jpg';
+
     test('debe mostrar el componente correctamente', () => {
-        let wrapper = shallow(<GifExpertApp />);
+        let wrapper = shallow(<GifExpertApp title={title} url={url}/>);
         expect(wrapper).toMatchSnapshot();
     })
     
