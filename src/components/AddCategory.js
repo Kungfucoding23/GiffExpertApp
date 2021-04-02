d869cfc0c34657bc470a7960531b343808a08e39
Alejandro <alesantin012@hotmail.com>
2021-04-02 20:38:26 -0300

    testing AddCategory

--- src/components/AddCategory.js
+++ src/components/AddCategory.js
@@ -6,11 +6,13 @@ export const AddCategory = ({setCategories}) => {
     const [inputValue, setInputValue] = useState('');
 
     const handleInputChange = (e) => {
-        setInputValue(e.target.value);
+        setInputValue(e.target.value);        
     }
 
     const handleSubmit = (e) => {
         e.preventDefault();
+
+        console.log('handleSubmit');
 
         if(inputValue.trim().length > 2){
             setCategories(category => [inputValue, ...category]);
@@ -22,6 +24,7 @@ export const AddCategory = ({setCategories}) => {
     return (
                     
         <form onSubmit={handleSubmit}>
+            <p>{inputValue}</p>
             <input 
                     type="text"
                     value={inputValue}
---
+++ src/tests/components/AddCategory.test.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import '@testing-library/jest-dom';
+import { shallow } from 'enzyme';
+import { AddCategory } from '../../components/AddCategory';
+
+describe('Pruebas en <AddCategory />', () => {
+    const setCategories = jest.fn();
+    let wrapper = shallow(<AddCategory setCategories={setCategories}/>);
+    beforeEach( () => {
+        jest.clearAllMocks();
+        wrapper = shallow(<AddCategory setCategories={setCategories}/>);
+    });
+    test('debe de mostrarse correctamente', () => {
+        expect(wrapper).toMatchSnapshot();
+    })
+
+    test('debe de cambiar la caja de texto', () => {
+        const input = wrapper.find('input');
+        const value = 'Ale';
+        input.simulate('change', { target: {value} });
+        expect(wrapper.find('p').text().trim()).toBe(value);
+    })
+    
+    test('no debe de postear la info con submit', () => {
+        wrapper.find('form').simulate('submit',{preventDefault(){}});
+        expect(setCategories).not.toHaveBeenCalled();
+    })
+    
+    test('debe de llamar el setCategories y limpiar la caja de texto', () => {
+        //simular el inputChange
+        const input = wrapper.find('input');
+        const value = 'Ale';
+        input.simulate('change', { target: {value} });
+        //simular el submit
+        wrapper.find('form').simulate('submit',{preventDefault(){}});
+        //setCategories se debe haber llamado
+        expect(setCategories).toHaveBeenCalled();
+        expect(setCategories).toHaveBeenCalledWith(expect.any(Function));
+        //el valor del input debe estar vacio
+        expect(wrapper.find('input').prop('value')).toBe('');        
+    })
+    
+    
+})
