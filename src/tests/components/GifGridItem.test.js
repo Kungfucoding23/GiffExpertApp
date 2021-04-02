eee14af69efeba0ace83f1cf2290a0f4020c2b2b
Alejandro <alesantin012@hotmail.com>
2021-04-02 19:39:48 -0300

    animate__animated

--- src/tests/components/GifGridItem.test.js
+++ src/tests/components/GifGridItem.test.js
@@ -7,10 +7,29 @@ describe('Pruebas en componente GifGridItem', () => {
 
     const title = 'Un titulo';
     const url = 'https://localhost/algo.jpg';
+    let wrapper = shallow(<GifExpertApp title={title} url={url}/>);
 
     test('debe mostrar el componente correctamente', () => {
-        let wrapper = shallow(<GifExpertApp title={title} url={url}/>);
         expect(wrapper).toMatchSnapshot();
     })
     
+    test('debe de tener un parrafo con el title', () => {
+        const p = wrapper.find('p');
+        expect(p.text().trim()).toBe(title);
+    })
+
+    test('debe de tener la imagen igual al url y alt de los props', () => {
+        const img = wrapper.find('img');
+        expect(img.prop('src')).toBe(url);
+        expect(img.prop('alt')).toBe(title);
+
+    })
+
+    test('debe de tener animate__animated', () => {
+        const div = wrapper.find('div');
+        const className = div.prop('className');
+
+        expect(className.includes('animate__animated')).toBe(true);
+    })
+
 })
